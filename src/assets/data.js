812dcd31185d4e210bfa450e3746ab41ccf3c452
Suarez Matias <suarezmatiasgonzalo@gmail.com>
2024-08-11 23:11:45 -0300

    se pasaron imagenes a web

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,34 +1,33 @@
 export const data = [
     {
-      id: 6, imgUrl: "src/assets/images/ACDC.jpg"
+      id: 6, imgUrl: "https://i.ibb.co/6DzbXFV/ZZTOP.jpg" //"src/assets/images/ACDC.jpg"
     },
     {
-      id: 7, imgUrl: "src/assets/images/KISS.jpg"
+      id: 7, imgUrl: "https://i.ibb.co/xhM73BT/ROLLING2.jpg" //"src/assets/images/KISS.jpg"
     },
     {
-      id: 8, imgUrl: "src/assets/images/ZZTOP.jpg"
+      id: 8, imgUrl: "https://i.ibb.co/R71dkk0/ROLLING.jpg" //"src/assets/images/ZZTOP.jpg"
     },
     {
-      id: 9, imgUrl: "src/assets/images/LEDZ.jpg"
+      id: 9, imgUrl: "https://i.ibb.co/vm8T0sr/QUEEN.jpg" //"src/assets/images/LEDZ.jpg"
     },
     {
-      id: 10, imgUrl: "src/assets/images/BEATTLES.jpg"
+      id: 10, imgUrl: "https://i.ibb.co/87FkTBV/NIRVANAa.jpg" //src/assets/images/BEATTLES.jpg"
     },
     {
-      id: 11, imgUrl: "src/assets/images/ROLLING.jpg"
+      id: 11, imgUrl: "https://i.ibb.co/Ry65XZf/LENNY.jpg"//src/assets/images/ROLLING.jpg"
     },
     {
-      id: 12, imgUrl: "src/assets/images/NIRVANAa.jpg"
+      id: 12, imgUrl: "https://i.ibb.co/Rzd8jcf/LEDZ.jpg" //"src/assets/images/NIRVANAa.jpg"
     },
     {
-      id: 13, imgUrl: "src/assets/images/QUEEN.jpg"
+      id: 13, imgUrl: "https://i.ibb.co/0FcYXMk/KISS.jpg" //"src/assets/images/QUEEN.jpg"
     },
     {
-      id: 14, imgUrl: "src/assets/images/ROLLING2.jpg"
+      id: 14, imgUrl: "https://i.ibb.co/bRpQjcc/BEATTLES.jpg" //"src/assets/images/ROLLING2.jpg"
     },
     {
-      id: 15, imgUrl: "src/assets/images/LENNY.jpg"
+      id: 15, imgUrl: "https://i.ibb.co/R0J25zR/ACDC.jpg" //"src/assets/images/LENNY.jpg"
     },
     
   ];
-
