be90399865bf68ac5d384424f82c069672cce15a
FerBarroso17 <ferbarroso6@gmail.com>
2024-08-11 21:59:25 -0300

    carga mod AlbumDetails.jsx

--- src/components/Albums/AlbumDetail.jsx
+++ src/components/Albums/AlbumDetail.jsx
@@ -15,37 +15,34 @@ function AlbumDetail() {
   };
 
   return (
-    <div className="album-detail-container">
+    <section className="fondo">
+      <div className="cont-volver">
       <button onClick={handleBackClick} className="btn-volver">
-        Volver
-      </button>
-      <div className="album-detail-header">
-        <img
-          src={cover || "/src/assets/mini_logo.jpg"}
-          alt={`${title} cover`}
-          className="album-detail-image"
-        />
-        <div className="album-detail-info">
-          <h1>{title}</h1>
-          <p>
-            <strong>Año: </strong> {year}
-          </p>
-          <p>
-            <strong>Artista: </strong>
-            <ArtistField artistId={artist} field="name" />
-          </p>
-          <p>
-            <strong>Biografia: </strong>
-            <ArtistField artistId={artist} field="bio" />
-          </p>
-          <a href="/Songs/create">
-            <label className="header-albums-btn-create">Agregar</label>
-          </a>
+          Volver a Albums
+        </button>
+      </div>
+      <div className="album-detail-container">
+        <div className="album-detail-header">
+          <img
+            src={cover || "/src/assets/mini_logo.jpg"}
+            alt={`${title} cover`}
+            className="album-detail-image"
+          />
+          <div className="album-detail-info">
+            <h1 className="titulo-album">{title}</h1>
+            <p>Año:  {year}
+            </p>
+            <p>Artista: <ArtistField artistId= {artist} field="name" /> </p>
+            <p className="album-detail-info-biografia">Biografia: <ArtistField artistId= {artist} field="bio" /> </p>
+            
+            <a href="/Songs/create">
+              <label className="header-albums-btn-create">Agregar canciones a este album</label>
+            </a>
+          </div>
         </div>
       </div>
-
       <AlbumSongs />
-    </div>
+    </section>
   );
 }
 
